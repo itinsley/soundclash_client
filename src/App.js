3dad059e2251ada473b32a5e66a4bf5555b41f95
Ian Tinsley <itinsley@gmail.com>
2018-10-10 16:43:18 +1100

    Refactor FB POC into Login component

--- src/App.js
+++ src/App.js
@@ -2,48 +2,9 @@ import React from 'react';
 import './App.css';
 import logo from './logo.svg';
 import { Nav, About, Home, Login, NotFound  } from './components';
-import FacebookLogin from 'react-facebook-login';
-import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-
 class App extends React.Component {
-  responseFacebook (response) {
-    const accessToken = response["accessToken"];
-    console.log(accessToken)
-
-    //See if we can proxy to api
-    // axios.get("/api/wontwork")
-
-    // See if we can get ourselves a session cookie
-    // Browser should take care of storage
-    axios.post('/users/auth/api_sessions', {
-        AccessToken: accessToken
-      })
-      .then((response)=> {
-        //This response confirms server validates FB user and returns FB user ID (user.uid in soundclash DB)
-        console.log(response);
-        //Now we've got a JWT. Can we access a secure route?
-        localStorage.token = response['data']['jwt']
-
-        axios.get('/users/83.json?jwt=bogus').then((response)=>{
-          console.log("bogus token should fail");
-          console.log(response);
-        })
-
-        axios.get('/users/83.json?jwt=' + localStorage.token).then((response)=>{
-          console.log(response);
-        })
-
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-  componentClicked (response) {
-    console.log(response)
-  }
 
   render() {
     return (
@@ -64,17 +25,6 @@ class App extends React.Component {
                 <Route component={NotFound}/>
               </Switch>
             </div>
-          </div>
-          <div id="fbDemo">
-            {process.env.REACT_APP_FACEBOOK_APPID}
-            <FacebookLogin
-              // appId="389386081241417" //soundclash_development
-              // appId="803392683074499" //soundclash_edge
-              appId={process.env.REACT_APP_FACEBOOK_APPID} //soundclash_edge
-              autoLoad={true}
-              fields="name,email,picture"
-              onClick={this.componentClicked}
-              callback={this.responseFacebook} />
           </div>
         </div>
       </Router>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
+import axios from 'axios';
 
 class Login extends Component {
   render() {
     return (
       <div>
         <h1>Login</h1>
+
+          <div id="fbDemo">
+            {process.env.REACT_APP_FACEBOOK_APPID}
+            <FacebookLogin
+              // appId="389386081241417" //soundclash_development
+              // appId="803392683074499" //soundclash_edge
+              appId={process.env.REACT_APP_FACEBOOK_APPID} //soundclash_edge
+              autoLoad={true}
+              fields="name,email,picture"
+              onClick={this.componentClicked}
+              callback={this.responseFacebook} />
+          </div>
+
       </div>
     );
   }
+
+ responseFacebook (response) {
+    const accessToken = response["accessToken"];
+    console.log(accessToken)
+
+    //See if we can proxy to api
+    // axios.get("/api/wontwork")
+
+    // See if we can get ourselves a session cookie
+    // Browser should take care of storage
+    axios.post('/users/auth/api_sessions', {
+        AccessToken: accessToken
+      })
+      .then((response)=> {
+        //This response confirms server validates FB user and returns FB user ID (user.uid in soundclash DB)
+        console.log(response);
+        //Now we've got a JWT. Can we access a secure route?
+        localStorage.token = response['data']['jwt']
+
+        axios.get('/users/83.json?jwt=' + localStorage.token).then((response)=>{
+          console.log(response);
+        })
+
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  componentClicked (response) {
+    console.log(response)
+  }
+
 }
 
 export default Login;
