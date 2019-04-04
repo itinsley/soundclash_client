5f699db896f889b93bd9f46499df1034c7ad37b2
Ian <itinsley@gmail.com>
2019-04-04 16:03:20 +1100

    Login to API using Facebook JWT

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,15 @@ import React from 'react';
 import {Route, BrowserRouter } from 'react-router-dom';
 import App from './App';
 import About from './components/About';
-import history from './history';
+import Login from './components/Login';
 
 export const makeMainRoutes = () => {
   return (
-    <BrowserRouter basename='/client' history={history}>
+    <BrowserRouter basename='/client' >
         <div>
           <Route path="/" render={(props) => <App {...props} />} />
           <Route path="/about" render={(props) => <About  {...props} />} />
+          <Route path="/login" render={(props) => <Login  {...props} />} />
         </div>
       </BrowserRouter>
   );
