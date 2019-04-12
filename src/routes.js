7c6bfd46eebcf18c462a3caf8b1450573ec5c029
Ian <itinsley@gmail.com>
2019-04-12 12:15:20 +1000

    Catch page for server routes

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,16 @@ import Logout from './components/Logout';
 import Clash from '../src/containers/Clash/ClashContainer'
 import {Navigation} from './containers/Navigation';
 
+  // Should never hit these routes in prod as root will point to server app
+  // and this app only has the scope of basename='/client'
+  function WrongWayGoBack(props){
+    return(
+      <div style={{paddingTop: "100px"}}>
+        Did you mean to visit {props.link}
+      </div>
+    )
+}
+
 export const makeMainRoutes = () => {
   return (
     <BrowserRouter basename='/client' >
@@ -17,6 +27,13 @@ export const makeMainRoutes = () => {
             <Route path="/login" render={(props) => <Login  />} />
             <Route path="/logout" render={(props) => <Logout />} />
             <Route path="/clashes/:clashId" render={(props) => <Clash {...props}/>} />
+            <Route path="/users/sign_up" render={(props) => {
+              return <WrongWayGoBack link={<a href='/users/sign_up'>Sign Up</a>} />
+            }} />
+            <Route path="/users/password/new" render={(props) => {
+              return <WrongWayGoBack link={<a href='/users/password/new'>Reset Password</a>} />
+            }} />
+            <Route path="/users/password/new" render={(props) => <Logout />} />
             <Route path="/" render={(props) => <Home  />} />
           </Switch>
         </div>
