8a2a7ec5421b3adf6a88ba26f86b0fdc9e7bf639
Ian <itinsley@gmail.com>
2019-04-26 14:47:41 +1000

    whitespace

--- src/containers/Clash/components/Comments/Comments.js
+++ src/containers/Clash/components/Comments/Comments.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 import CommentApi from "../../../../api/Comments";
 import CommentItem from "./CommentItem";
 import Avatar from "../../../../components/Avatar";
- 
+
 class Comment extends Component{
   constructor(props){
     super(props);
@@ -24,7 +24,7 @@ class Comment extends Component{
   handleChange(e) {
     this.setState({[e.target.name]:e.target.value});
   }
-  
+
   renderHistory(){
     const comments = this.state.comments.map((comment)=>{
       if (comment.new){
@@ -37,7 +37,7 @@ class Comment extends Component{
       } else
         return (
           <CommentItem key={`comment=${comment.id}`} comment={comment} />
-        )  
+        )
     })
     return comments;
   }
@@ -64,7 +64,7 @@ renderCommentForm(){
       name: this.props.currentUser.userName,
       image_url : this.props.currentUser.imageUrl
     }
-    
+
     return(
       <form onSubmit={this.handleSubmit}>
         <div className="row py-2 px-0 mx-0">
@@ -74,22 +74,22 @@ renderCommentForm(){
                       size='60' />
           </div>
           <div className='col text-left px-0 mx-0' style={{width:'100%'}}>
-                <textarea type="text" 
+                <textarea type="text"
                         rows="2"
-                        value={this.state.newComment} 
-                        className="form-control" 
+                        value={this.state.newComment}
+                        className="form-control"
                         name="newComment"
                         placeholder="Comment on this track"
-                        onChange={this.handleChange} /> 
+                        onChange={this.handleChange} />
           </div>
-        </div>        
+        </div>
         <div className="row py-0 px-0 mx-0 justify-content-end" >
           <div className='col-auto px-0'>
             <input className="btn btn-dark btn-sm" type="submit" value="Post" />
           </div>
         </div>
-      </form>  
-    )  
+      </form>
+    )
   } else{
     return (
       <span>
