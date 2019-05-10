dacc3204aa9dfaa23832520228629c3327be56c6
Ian <itinsley@gmail.com>
2019-05-10 15:20:39 +1000

    Update assertTextInElements.js

--- test/helpers/assertTextInElements.js
+++ test/helpers/assertTextInElements.js
@@ -1,6 +1,7 @@
 const assert = require("assert");
 
 async function assertTextInElements(browser, cssSelector, innerText){
+  //This is a very slow function for some reason
   const result = await browser.elements('css selector', cssSelector);
   const elements = result.value;
 
@@ -9,6 +10,7 @@ async function assertTextInElements(browser, cssSelector, innerText){
     const res = await browser.elementIdAttribute(elementId, 'innerText');
     const elementInnerText = res.value;
     if (elementInnerText===innerText){
+      console.log(`Found ${innerText} in ${cssSelector}`)
       return;
     }
   }
