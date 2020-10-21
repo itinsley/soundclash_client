58737220b4a3cac711329d8ac6697cb3f158ef48
Ian <itinsley@gmail.com>
2020-10-21 15:34:36 +1100

    Auth0 refresh and state - savepoint
     - Copy pattern from auth0-spa sample to fix refresh issues
     - Note Google login doesn't retain state on refresh - this may be a localhost thing

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import Loading from "./components/Loading";
+import NavigationContainer from './app/Navigation/NavigationContainer';
+import Home from './app/Home/HomeContainer';
+import About from './app/About/About';
+import Clash from '../src/app/Clash/ClashContainer';
+import { useAuth0 } from "@auth0/auth0-react";
+import history from "./history";
+
+// styles
+import "./App.css";
+import { Provider } from "react-redux";
+
+// Store
+import reducer from './reducer';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+const store = createStore(reducer, applyMiddleware(thunk));
+
+
+const App = () => {
+  const { isLoading, error } = useAuth0();
+
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        <div id="app" >
+          <NavigationContainer />
+            <Switch>
+              <Route path="/about" render={(props) =>  <About  />} />
+              <Route path="/clashes/:clashId" render={(props) => <Clash {...props}/>} />
+              <Route path="/" render={(props) => <Home />} />
+            </Switch>
+        </div>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import 'bootstrap/dist/css/bootstrap.css';
-import { makeMainRoutes } from './routes';
-import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import reducer from './reducer';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Auth0Provider } from "@auth0/auth0-react";
+import config from "./auth_config.json";
+import history from "./history";
 
-// Global Redux Store
-const store = createStore(reducer, applyMiddleware(thunk));
 
-const routes = makeMainRoutes(store);
+const onRedirectCallback = (appState) => {
+  history.push(
+    appState && appState.returnTo
+      ? appState.returnTo
+      : window.location.pathname
+  );
+};
 
 ReactDOM.render(
-  routes,
-  document.getElementById('root')
+  <Auth0Provider
+    domain={config.domain}
+    clientId={config.clientId}
+    audience={config.audience}
+    redirectUri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
